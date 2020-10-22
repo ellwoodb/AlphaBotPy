7e89e9f61141c1d0637785a77a383976a528f63e
ellwoodb <56759474+matthew230506@users.noreply.github.com>
2020-10-22 19:12:39 +0200

    Added scheduling and timed messages (V 0.1.1)

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 from discord.ext.commands import Bot as BotBase
 from discord import Embed , File
 from discord.ext.commands import CommandNotFound
+
+from ..db import db
 
 PREFIX = "+"
 OWNER_IDS = [752492641970814986]
@@ -17,6 +20,8 @@ class Bot(BotBase):
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
+        db.autosave(self.scheduler)
+
         super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
 
     def run(self, version):
@@ -27,6 +32,10 @@ class Bot(BotBase):
 
         print("running bot...")
         super().run(self.TOKEN, reconnect=True)
+
+    #async def print_message(self):
+    #    channel = self.get_channel(599569584005185539)
+    #    await channel.send("Timed Test.")
 
     async def on_connect(self):
         print("bot connected")
@@ -55,29 +64,32 @@ class Bot(BotBase):
         if not self.ready:
             self.ready = True
             self.guild = self.get_guild(503104675256729600)
-            print("bot ready")
+            #self.scheduler.add_job(self.print_message, CronTrigger(second="0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55"))
+            self.scheduler.start()
 
             #define channel to send message in
             channel = self.get_channel(599569584005185539)
-
             #Send message
             await channel.send("Ich bin online!")
 
             #Send embed
-            embed = Embed(title="Online", description="Alpha Bot ist online!", colour=0xFF0000, timestamp=datetime.utcnow())
-            fields = [("Name", "Value", True), 
-                      ("Test", "Test", True),
-                      ("Test2", "Test2", False)]
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-            embed.set_footer(text="Footer Test")
-            embed.set_author(name="Alpha Bot", icon_url=self.guild.icon_url)
-            embed.set_thumbnail(url=self.guild.icon_url)
-            embed.set_image(url=self.guild.icon_url)
-            await channel.send(embed=embed)
+            #embed = Embed(title="Online", description="Alpha Bot ist online!", colour=0xFF0000, timestamp=datetime.utcnow())
+            #fields = [("Name", "Value", True), 
+            #          ("Test", "Test", True),
+            #          ("Test2", "Test2", False)]
+            #for name, value, inline in fields:
+            #    embed.add_field(name=name, value=value, inline=inline)
+            #embed.set_footer(text="Footer Test")
+            #embed.set_author(name="Alpha Bot", icon_url=self.guild.icon_url)
+            #embed.set_thumbnail(url=self.guild.icon_url)
+            #embed.set_image(url=self.guild.icon_url)
+            #await channel.send(embed=embed)
 
             #Send file
-            await channel.send(file=File("./data/images/burger_test.jpg"))
+            #await channel.send(file=File("./data/images/burger_test.jpg"))
+
+
+            print("bot ready")
 
         else:
             print("bot reconnected")
